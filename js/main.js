f84895ae001a73850c879424f2fb717763772f05
KokiSato008800 <kouki00800@gmail.com>
2021-02-04 09:37:12 +0900

    week3 level.04

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-alert('Good Night World');
+// varを使用
+console.log('--------------varを使用--------------');
+var Var = '初期値';
+console.log('1.' + Var);
+Var = '代入';
+console.log('2.' + Var);
+var Var = '再宣言';
+console.log('3.' + Var);
+
+// letを使用
+console.log('--------------letを使用--------------');
+let Let = '初期値';
+console.log('1.' + Let);
+Let = '代入';
+console.log('2.' + Let);
+
+// constを使用
+console.log('--------------constを使用--------------');
+const Const = '初期値';
+console.log('1.' + Const);
+
+
